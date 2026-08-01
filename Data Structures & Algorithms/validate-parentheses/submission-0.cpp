b2793324c4ee9e1248007534b8bf5850e2c16bdc
Milind Pius <152724522+MilindPius2005@users.noreply.github.com>
2026-08-01 21:29:04 +0530

    Bulk sync: 11 submissions

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>stack;
+        unordered_map<char, char>closetoopen={
+            {')','('},
+            {']','['},
+            {'}','{'}
+        };
+        for(char c:s){
+            if(closetoopen.count(c)){
+                if(!stack.empty() && stack.top()==closetoopen[c]){
+                    stack.pop();
+                } else{
+                    return false;
+                }
+            }else{
+                stack.push(c);
+            }
+        }
+        return stack.empty();
+
+
+        
+        
+    }
+};
